2c528f1ba9fc5cc49080611f424b82253dc8ca99
tinhh <tinhhdev@gmail.com>
2024-04-08 21:02:02 +0700

    first commit

---
+++ src/main/java/vn/tinhh/utils/core/UtilsCoreApplication.java
@@ -0,0 +1,13 @@
+package vn.tinhh.utils.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilsCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/tinhh/utils/core/UtilsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.tinhh.utils.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
